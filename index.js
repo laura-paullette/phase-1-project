72bcd89bb1715d72926adf43b2600e848ec9f992
laura-paullette <laura.paullette@student.moringaschool.com>
2023-04-06 03:57:21 +0300

    complete

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 const apiUrl = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
-const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI ="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+const imagePath = "https://image.tmdb.org/t/p/w1280";
+const apiSearch ="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
 // Selecting our Elements.
+
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
-/* call the showMovies function that requests the movie data from the Api using fetch.
- Then it puts those data in the main HTML tag by creating elments for those data. */
+
+// call the showMovies function that will request the movie data from the Api using fetch.
+//Then it puts those data in the main HTML tag by creating elements for those data. 
+
 showMovies(apiUrl);
 function showMovies(url){
     fetch(url).then(res => res.json())
     .then(function(data){
     data.results.forEach(element => {
-      // Creating elemnts for our data inside the main tag. 
+
+    // Creating elements for the data inside the main tag. 
         const el = document.createElement('div');
         el.setAttribute('class', 'movies')
         const image = document.createElement('img');
         const text = document.createElement('h2');
 
         text.innerHTML = `${element.title}`;
-        image.src = IMGPATH + element.poster_path;
+        image.src = imagePath + element.poster_path;
         el.appendChild(image);
         el.appendChild(text);
         main.appendChild(el);
     });
+
+    //Adding an event litener on click 
+
     let movies = document.querySelectorAll('.movies')
     console.log('MOVIE: ', movies)
     for(let movie of movies){
@@ -48,7 +56,7 @@ function showMovies(url){
             }
 
             
-
+            // Creating elements for the movie details
             let poster = document.createElement('div')
             let posterPic = document.createElement('img')
             poster.style = 'position: relative; height: 100%; width: 30%; background: white;'
@@ -77,6 +85,8 @@ function showMovies(url){
             detail.appendChild(heading)
             detail.appendChild(description)
 
+            //Movie description details
+
             fetch(search)
             .then(res => res.json())
             .then(data=>{
@@ -103,22 +113,16 @@ function showMovies(url){
 
 
 
-
-
-
-
-
-
 // Prevent the Form from submitting if the search bar is empty.
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    main.innerHTML = '';
-     
+    main.innerHTML = '';  
     const searchTerm = search.value;
- /* Adding the value wriiten in the search bar to the search Api,
-    in order to get the movies we search for. */
+
+
+ //Adding the value written in the search bar to the search Api, in order to get the movies we search for. 
     if (searchTerm) {
-        showMovies(SEARCHAPI + searchTerm);
+        showMovies(apiSearch + searchTerm);
         search.value = "";
     }
 });
